7097026ed93fdd22b24c0352240ac084ee861f29
Gustavo Maciel <79233238+Gus-1003@users.noreply.github.com>
2022-04-12 12:08:57 -0300

    Update Aula2Controller.java

--- src/main/java/ufrn/com/prog/Aula2Controller.java
+++ src/main/java/ufrn/com/prog/Aula2Controller.java
@@ -2,15 +2,33 @@ package ufrn.com.prog;
 
 import java.io.IOException;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class Aula2Controller {
+
+
+    @Autowired
+    ServletContext servletContext;
+
+    @RequestMapping("/redirecionar")
+    public void doRedirecionamento(HttpServletRequest request, HttpServletResponse response) throws IOException{}
+    
+    @RequestMapping("/parte2")
+    public void doParte2(HttpServletRequest request, HttpServletResponse response) throws IOException{}
+
+    @RequestMapping("/parte1")
+    public void doParte1(HttpServletRequest request, HttpServletResponse response) throws IOException{}
+
+    @RequestMapping(value = "/principal", method = RequestMethod.GET)
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws SevletException{}
 
     @RequestMapping(value = "/cadastraform", method = RequestMethod.GET)
     public void doCadastraForm(HttpServletRequest request, HttpServletResponse response) throws IOException {
